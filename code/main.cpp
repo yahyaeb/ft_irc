7179bcacf2d60f2ec0e6ec65e1d6e5f47c3e4fbf
ihhadjal <ihebhdj42@gmail.com>
2025-09-30 17:32:42 +0200

    arguments parsing

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,44 @@
 #include "../resources/Irc.hpp"
 
 
-int main ()
+int main (int argc, char **argv)
 {
-    std::cout << "test iheb\n";
+    if (argc != 3 || !parsePortAndPswd(argv[1], argv[2]))
+    {
+        std::cout << "womp womp\n";
+        exit (1);
+    }
+    else
+    {
+        std::cout << "parse ok\n";
+    }
+}
+
+bool checkPortNb(int portNb)
+{
+    if (portNb >= 1 && portNb <= 65535)
+        return true;
+    else
+        return false;
+    return true;
+}
+
+bool parsePortAndPswd(char *port, char *password)
+{
+    int portNb = atoi(port);
+    if (strlen(port) == 0 || strlen(password) == 0 || strlen(password) >= 255)
+        return false;
+    for (int i = 0; port[i]; i++)
+    {
+        if (!isdigit(port[i]) || port[i] == '+' 
+            || port[i] == '-' || port[i] == ' '
+            || port[i] == '\t' || !checkPortNb(portNb))
+            return false;
+    }
+    for (int i = 0; password[i]; i++)
+    {
+        if (!isprint(password[i]))
+            return false;
+    }
+    return true;
 }
--- resources/Irc.hpp
+++ resources/Irc.hpp
@@ -1,5 +1,8 @@
 #ifndef IRC_HPP
 #define IRC_HPP
-
+#include <vector>
 #include <iostream>
+#include <stdlib.h>
+
+bool parsePortAndPswd(char *port, char *password);
 #endif
